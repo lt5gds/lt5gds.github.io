330360b21d4aa635810b0e3a247410218ec3cd95
esaputras354 <esaputras_siskom14@mahasiswa.unj.ac.id>
2017-09-27 02:59:51 +0700

    designer was here

--- list-artikel.js
+++ list-artikel.js
@@ -2,7 +2,7 @@
 //tambahin nana file di dalem [] jangan lupa koma dan ''
 
 
-var list=['index','highconcept','Rangkuman Dokumentasi Libgdx','test libgdx','OnePageDesignTemplate','StoryBibleTemplate'];
+var list=['index','highconcept','Rangkuman Dokumentasi Libgdx','test libgdx','OnePageDesignTemplate','StoryBibleTemplate','CTaylorDesign'];
 
 
 
